7fec5492c2d3ea69e04c6019b103b8fdf8882d32
Rohit Khati <50332813+ro-hit81@users.noreply.github.com>
2020-07-02 22:47:10 +0545

    Server side registering process.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -24,7 +24,7 @@ export default {
                 email: this.email,
                 password: this.password
             })
-            console.log(response);
+            console.log(response.data)
         }
     }
 }
