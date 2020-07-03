fabf64528bb78d2dc972297175dd5fdb96f6d98e
Rohit Khati <50332813+ro-hit81@users.noreply.github.com>
2020-07-03 20:04:03 +0545

    Desiginig frontend of  Registering component.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,11 +1,57 @@
 <template>
     <div>
-        <h1>Register</h1>
-        <input type="email" name="email" v-model="email" placeholder="email"/> <br>
-        <input type="password" name="password" v-model="password" placeholder="password"/> <br>
-        <div class="error" v-html="error"/>
-        <br>
-        <button @click="register">Register</button>
+        <v-card max-width="800px" class="mx-auto">
+            <v-toolbar class="cyan lighten-2" dark flat>
+                <v-spacer></v-spacer>
+                <v-toolbar-title>Be a Part of Playlist Vue App</v-toolbar-title>
+                <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-container>
+                <v-row>
+                    <v-col cols="8" class="mx-auto">
+                        <v-text-field
+                            label="Email address"
+                            placeholder="Enter your email id here !!!"
+                            type="email"
+                            prepend-inner-icon="mdi-email"
+                            v-model="email"
+                            color="cyan lighten-1"
+                            hint="e.g. YourEmail@Smth.com"
+                            rounded
+                            outlined
+                        >
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="8" class="mx-auto">
+                        <v-text-field
+                            label="Password"
+                            placeholder="Provide strong password"
+                            type="password"
+                            prepend-inner-icon="mdi-lock-question"
+                            v-model="password"
+                            color="cyan lighten-1"
+                            hint="Please do not use your original password. You can use: 1a2b3c4d"
+                            rounded
+                            outlined
+                        >
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+                <div>
+                    <span>{{error}}</span>
+                </div>
+                <v-row>
+                    <v-col cols="2" class="mx-auto">
+                        <v-btn ripple rounded class="cyan lighten-1 white--text" @click="register">
+                            <v-icon class="mr-3">mdi-door-open</v-icon>
+                            <span>Register</span>
+                        </v-btn>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
     </div>
 </template>
 
@@ -15,8 +61,8 @@ import AuthenticationService from '@/services/AuthenticationService'
 export default {
     data() {
         return{
-            email: 'email here !!!',
-            password: '12345678',
+            email: '',
+            password: '',
             error: null,
         }
     },
@@ -34,10 +80,3 @@ export default {
     }
 }
 </script>
-
-<style scoped>
-.error{
-    color: red
-}
-
-</style>
