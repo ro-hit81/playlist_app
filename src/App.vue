c7961f8a95f39a26059e4d51c11b428d0dd95884
Rohit Khati <50332813+ro-hit81@users.noreply.github.com>
2020-07-03 16:44:14 +0545

    Managing initial stage of vuetify plugin.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  }),
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
